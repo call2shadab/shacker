41504c97a0dc651fa7d4aa67f0f89d16fd7d97f3
call2shadab <call2shadab@gmail.com>
2019-01-17 19:36:01 +0530

    JSON File

---
+++ main.js
@@ -0,0 +1,18 @@
+var myGirl = [ 
+	{
+		"name": "Tina",
+		"fav-color": "black",
+		"fav-food": "burger"
+	},
+	{
+		"name": "Subhadra",
+		"fav-color": "blue",
+		"fav-food": "pizza"
+	}
+	{
+		"name": "Sangeeta",
+		"fav-color": "red",
+		"fav-food": "biryani"
+	}
+]
+
