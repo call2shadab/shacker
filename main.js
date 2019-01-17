47b85e228b99a38fc8bb585cceb2fc2a0ef20f80
Shadab Ansari <call2shadab@gmail.com>
2019-01-17 20:09:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ var myGirl = [
 		"name": "Subhadra",
 		"fav-color": "blue",
 		"fav-food": "pizza"
-	}
+	},
 	{
 		"name": "Sangeeta",
 		"fav-color": "red",
 		"fav-food": "biryani"
 	}
-]
+];
 
